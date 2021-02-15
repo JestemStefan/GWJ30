3281efff68ab23637d68c9a0e984d738fbfa6512
Grid Sherman <gtsherm@gmail.com>
2021-02-15 18:33:23 -0500

    reanimated most stuff, still some improvements to make but gotta do the next thing

--- GR_assets/Player/Player.gd
+++ GR_assets/Player/Player.gd
@@ -22,6 +22,7 @@ var impact_velocity: Vector3 = Vector3.ZERO
 var vertical_velocity: float = 0.0
 var current_velocity: Vector3 = Vector3.ZERO
 var is_grounded: bool = false
+var is_sliding: bool = false
 var current_weapon = null
 var wep_cont_node = null
 
@@ -37,6 +38,7 @@ onready var floor_check = $FloorCast
 onready var anim_tree = $AnimationTree
 onready var melee_shape = $MeleeShape
 onready var hud = $HUD
+
 # Utils
 var input_amount = 1.0
 var first_person_offset
@@ -44,6 +46,7 @@ var third_person_offset
 onready var normal_material = preload("res://GR_assets/Player/Player_spatialmaterial.tres")
 onready var invis_material = preload("res://GR_assets/Player/Invisible_spatialmaterial.tres")
 onready var space_state = get_world().get_direct_space_state()
+var new_ik_interp: float
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -55,9 +58,10 @@ func _ready():
 			print(current_weapon)
 			current_weapon = wep_cont.get_child(0)
 			print(current_weapon)
+	anim_tree.active = true
 	# Set up IK
 	right_hand_ik.start()
-	left_hand_ik.start()
+	#left_hand_ik.start()
 
 func _physics_process(delta):
 	heart_rate -= delta
@@ -73,15 +77,20 @@ func _physics_process(delta):
 		input_vector.x -= input_amount
 	input_vector = -input_vector.normalized()
 	rotated_input_vector = input_vector.rotated(Vector3.UP, camera_controller.global_transform.basis.get_euler().y + PI)
-	if impact_velocity.length() > 16.0:
+	# Handle sliding
+	if impact_velocity.length() > 12.0:
+		is_sliding = true
 		desired_velocity = impact_velocity
 		anim_tree.set("parameters/dodge_blend/blend_amount", impact_velocity.length() / dodge_speed)
 		if floor_check.is_colliding():
 			Utils.instantiate(load("res://GR_assets/Effects/DustEffect.tscn"),  self.global_transform.origin, Vector3.UP)
 	else:
+		is_sliding = false
 		desired_velocity = lerp(desired_velocity, rotated_input_vector * move_speed, 1.0 - pow(0.01, delta))
 		anim_tree.set("parameters/dodge_blend/blend_amount", 0.0)
+	# Slow sliding speed down
 	impact_velocity = lerp(impact_velocity, Vector3.ZERO, 1.0 - pow(0.1, delta))
+	# Add up/down logic
 	vertical_velocity_logic(delta)
 	# Add everything together and move
 	var final_move = desired_velocity + Vector3.UP * vertical_velocity - get_floor_normal() * 4.0
@@ -96,15 +105,18 @@ func _physics_process(delta):
 		anim_tree.set("parameters/run_speed/scale", (move_speed / max_move_speed) * 2.5)
 		anim_tree.set("parameters/jump_blend/blend_amount", 0.0)
 	else:
-		anim_tree.set("parameters/jump_blend/blend_amount", 1.0)
+		var current_jump_blend = anim_tree.get("parameters/jump_blend/blend_amount")
+		anim_tree.set("parameters/jump_blend/blend_amount", lerp(current_jump_blend, 1.0, 5.0 * delta))
 		anim_tree.set("parameters/jump_up_down/blend_position", vertical_velocity / jump_speed)
-		
+	
 	if anim_tree.get("parameters/melee/active") == false:
-		right_hand_ik.interpolation = 0.9
-		left_hand_ik.interpolation = 0.9
+		new_ik_interp = 1.0 - anim_tree.get("parameters/dodge_blend/blend_amount")
 	anim_tree.set("parameters/heart_rate/scale", heart_rate / 50.0)
 	hud.set_heart_rate(heart_rate)
 	update_movement_vars()
+	# Finalize the IK interp so its always smooth
+	right_hand_ik.interpolation = lerp(right_hand_ik.interpolation, new_ik_interp, 3.0 * delta)
+	left_hand_ik.interpolation = lerp(left_hand_ik.interpolation, new_ik_interp, 3.0 * delta)
 
 func action_inputs():
 	if Input.is_action_just_pressed("jump") and floor_check.is_colliding():
@@ -113,9 +125,9 @@ func action_inputs():
 		if current_weapon != null:
 			if current_weapon.fire(camera_controller.aim_position):
 				camera_controller.recoil += current_weapon.recoil
-	if Input.is_action_pressed("melee") and anim_tree.get("parameters/melee/active") == false:
+	if Input.is_action_pressed("melee") and anim_tree.get("parameters/melee/active") == false and not is_sliding:
 		do_melee_attack()
-	if Input.is_action_just_pressed("dodge") and impact_velocity.length() < 16.0:
+	if Input.is_action_just_pressed("dodge") and not is_sliding:
 		dodge(rotated_input_vector)
 
 func dodge(direction: Vector3):
@@ -125,12 +137,12 @@ func dodge(direction: Vector3):
 	self.vertical_velocity = 4.0
 
 func do_melee_attack():
-	right_hand_ik.interpolation = 0.125
-	left_hand_ik.interpolation = 0.125
+	right_hand_ik.interpolation = 0.02
+	left_hand_ik.interpolation = 0.2
 	#right_hand_ik.stop()
 	#left_hand_ik.stop()
 	anim_tree.set("parameters/melee/active", true)
-	self.impact_velocity = self.global_transform.basis.z * 12.0
+	#self.impact_velocity = self.global_transform.basis.z * 12.0
 	if is_equal_approx(anim_tree.get("parameters/melee_choice/blend_amount"), 0.0):
 		anim_tree.set("parameters/melee_choice/blend_amount", 1.0)
 	else:
@@ -150,8 +162,9 @@ func melee_do_hit():
 		if not is_grounded:
 			#vertical_velocity = 10.0
 			pass
-	#for hit in hits:
-	#	hit["collider"].take_damage(self.global_transform.origin, body.global_transform.basis.z, dmg, dmg_type, 0.0)
+	for hit in hits:
+		if hit["collider"].has_method("take_damage"):
+			hit["collider"].take_damage(self.global_transform.origin, self.global_transform.basis.z, 10.0)
 
 func vertical_velocity_logic(delta):
 	# If our head hit something, end jump
