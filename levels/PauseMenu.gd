3d7eba145e1d1fb43685d4d86b4d10543ecf6905
Grid Sherman <gtsherm@gmail.com>
2021-02-16 17:39:43 -0500

    merged the pausemenu stuff

--- levels/PauseMenu.gd
+++ levels/PauseMenu.gd
@@ -1,5 +1,16 @@
 extends CenterContainer
 
+func _input(event):
+	if(event.is_action_pressed("ui_cancel")):
+		var pause_menu = self
+		if pause_menu.is_visible_in_tree():
+			pause_menu.hide()
+			get_tree().paused = false
+		else:
+			pause_menu.show()
+			get_tree().paused = true
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
 
 func _on_ExitButton_pressed():
 	get_tree().paused = false
