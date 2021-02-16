45318aac55bb950f04ee310a730afe8a0c306c56
JestemStefan <pudipudi2x@gmail.com>
2021-02-16 15:54:46 +0100

    Post-merge fix: Disconnected signals

--- src/EnemyClass.gd
+++ src/EnemyClass.gd
@@ -17,20 +17,21 @@ func _ready():
 	player = get_tree().get_nodes_in_group("Player")[0]
 	
 func _physics_process(delta):
+	
 	player_position = player.global_transform.origin
 	
 	# If there are still points to go
 	if path_node < path.size():
-		
 		# get direction to next point
 		var dir:Vector3 = (path[path_node] - global_transform.origin)
 		
 		# if you are very close to the point then select next point
 		if dir.length_squared() < 5:
 			path_node += 1
-		
+			
 		# move to point
 		else:
+			
 			process_movement(dir)
 			
 			
