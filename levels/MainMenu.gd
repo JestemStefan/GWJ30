9efa249f814675e732ba89b30e7d01dc334a7ff0
Devin Cormier <devin.cormier@gmail.com>
2021-02-17 23:38:04 -0500

    Add options, mouse sensitivity

--- levels/MainMenu.gd
+++ levels/MainMenu.gd
@@ -1,20 +1,8 @@
 extends MarginContainer
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 
 func _on_ExitButton_pressed():
@@ -23,3 +11,7 @@ func _on_ExitButton_pressed():
 
 func _on_PlayButton_pressed():	
 	get_tree().change_scene("res://levels/Sandbox1.tscn")
+
+
+func _on_OptionsButton_pressed():
+	get_tree().change_scene("res://levels/OptionsMenu.tscn")
