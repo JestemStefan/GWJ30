7e708cd720fd7d32ffd8f868113a077d98793e04
Devin Cormier <devin.cormier@gmail.com>
2021-02-19 10:21:18 -0500

    Add screen settings to options menu

--- levels/OptionsMenu.gd
+++ levels/OptionsMenu.gd
@@ -1,9 +1,17 @@
 extends Control
+onready var screen_settings_option_button = $CenterContainer/VBoxContainer/ScreenSettingsContainer/CenterContainer/ScreenSettingsOptionButton
 
 
 func _ready():
 	var mouse_sensitivity_slider = $CenterContainer/VBoxContainer/HBoxContainer/CenterContainer/MouseSensitivitySlider
 	mouse_sensitivity_slider.value = Globals.MOUSE_SENSITIVITY * 50.0	
+	
+	for item in Globals.SCREEN_SETTINGS:
+		screen_settings_option_button.add_item(item)
+	
+	#OS.window_borderless = true
+	#OS.window_fullscreen = true
+	
 
 func _on_HSlider_value_changed(value):
 	Globals.MOUSE_SENSITIVITY = value / 50.0
@@ -11,3 +19,17 @@ func _on_HSlider_value_changed(value):
 
 func _on_CloseButton_pressed():
 	get_tree().change_scene("res://levels/MainMenu.tscn")
+
+
+func _on_ScreenSettingsOptionButton_item_selected(index):
+	match Globals.SCREEN_SETTINGS[index]:
+		"Full Screen Windowed":
+			OS.window_fullscreen = true
+			OS.window_borderless = false
+		"Full Screen":
+			OS.window_fullscreen = true
+			OS.window_borderless = true
+		"Windowed":
+			OS.window_fullscreen = false
+			OS.window_borderless = false
+		
--- src/Globals/Globals.gd
+++ src/Globals/Globals.gd
@@ -2,3 +2,10 @@ extends Node
 
 var scene_root = null
 var MOUSE_SENSITIVITY = 1
+
+var SCREEN_SETTINGS = [
+	"Full Screen Windowed", 
+	"Full Screen",
+	"Windowed"
+]
+var SCREEN_SETTING = "Windowed"
