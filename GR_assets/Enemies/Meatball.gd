3abc9d1db69e265c753d0f40744b68c129520f30
JestemStefan <37214990+JestemStefan@users.noreply.github.com>
2021-02-21 19:11:05 +0100

    Added particles for Meatball and egg

--- GR_assets/Enemies/Meatball.gd
+++ GR_assets/Enemies/Meatball.gd
@@ -2,6 +2,7 @@ extends RigidBody
 class_name Meatball
 
 onready var leech = preload("res://GR_assets/Enemies/Leech.tscn")
+onready var pop_smoke = preload("res://GR_assets/Effects/PopSmoke.tscn")
 
 var direction: Vector3 = Vector3.ZERO
 var velocity: int = 30
@@ -13,9 +14,16 @@ func _ready():
 
 
 func _on_Meatball_body_entered(body):
+	
 	if body.is_in_group("Floor"):
+		spawn_smoke()
 		var spawn = leech.instance()
 		get_parent().add_child(spawn)
 		spawn.set_global_transform(global_transform)
 		
 		call_deferred("free")
+
+func spawn_smoke():
+	var smoke = pop_smoke.instance()
+	get_parent().add_child(smoke)
+	smoke.set_global_transform(global_transform)
